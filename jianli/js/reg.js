bc2c254277d28e28a91ee039da98fa1fdd542517
lxg <137981134@qq.com>
2017-03-08 22:37:00 +0800

    2

--- jianli/js/reg.js
+++ jianli/js/reg.js
@@ -1,2 +1,2 @@
-/* reg.js Date:2017-03-08 20:39:56 */
+/* reg.js Date:2017-03-08 22:29:54 */
 define("js/reg.js",[],function(require,a,b){var c={yanzheng:function(){var a=/^[\u4e00-\u9fa5]+$/;var b=$("#jbxxi .xingming").val();!a.test(b)}};b.exports={init:c.yanzheng}});
